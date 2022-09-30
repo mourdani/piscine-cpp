465dcdc8733f6d9998ec8065801eac7500237981
mourdani <mourdani@student.42.fr>
2022-09-30 20:14:34 +0200

    fix main

--- day01/ex03/main.cpp
+++ day01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mourdani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 07:07:33 by mourdani          #+#    #+#             */
-/*   Updated: 2022/09/30 19:27:17 by mourdani         ###   ########.fr       */
+/*   Updated: 2022/09/30 20:14:16 by mourdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,21 @@
 
 int main()
 {
-/*	Weapon gun = Weapon("GUN");
-	std::cout << gun.getType() << std::endl; 
-	gun.setType("mitrallaite");
-	std::cout << gun.getType() << std::endl; 
-	return 0;*/
-	Weapon club = Weapon("crude spiked club");
-	HumanA bob("Bob", club);
-	bob.attack();
-	club.setType("some other type of club");
-	bob.attack();
-	HumanB jim("Jim");
-	jim.setWeapon(club);
-	jim.attack();
-	club.setType("some other type of club");
-	jim.attack();
+	{
+		Weapon  club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon  club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+
 	return 0;
 }
