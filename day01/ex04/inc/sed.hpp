a80eb2c245ac13079451ab3e1af1c712b4f31536
mourdani <meziane.ourdani@protonmail.com>
2022-10-22 01:15:33 +0200

    add ex04

---
+++ day01/ex04/inc/sed.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sed.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mourdani <mourdani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 00:28:29 by mourdani          #+#    #+#             */
+/*   Updated: 2022/10/06 23:16:40 by mourdani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef SED_HPP
+#define SED_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream> // File stream lib
+#include <unistd.h>
+
+#endif
---
+++ day01/ex04/srcs/main.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mourdani <mourdani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 00:24:05 by mourdani          #+#    #+#             */
+/*   Updated: 2022/10/07 03:42:46 by mourdani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/sed.hpp"
+
+std::string	sed(std::string buffer, std::string str1,  std::string str2){
+
+	int i = 0; // index
+	while (1) {
+		// get index of found str1, break otherwise
+		if ((i = buffer.find(str1)) == -1) break;
+		// Erase occurence of str1 at index
+		buffer.erase(i, str1.length());
+		// Insert str2 at index 
+		buffer.insert(i, str2);
+	}
+	return buffer;
+}
+
+int	main(int ac, char **av) {
+
+	// Intitialise variables
+	std::string filename(av[1]);
+	std::string str1(av[2]);
+	std::string str2(av[3]);
+	std::string buffer;
+
+	// create file object for original file
+	std::ifstream ofile(av[1]);
+
+	// check for errors
+	if (str1.empty() || ac != 4 || ofile.fail()) return 1;
+
+	// Loop to store content of file object to buffer
+	while (ofile.good()) buffer += ofile.get();
+
+	// Create cfile by appending ".replace" to ofile
+	std::ofstream cfile((filename.append(".replace")).c_str(), std::ofstream::out);
+
+	// Replace and append to cfile
+	cfile << sed(buffer, av[2], av[3]);
+
+	std::cout << "Replacement done succesfully!\n";
+
+	// close files
+	ofile.close();
+	cfile.close();
+
+	return 0;
+}
