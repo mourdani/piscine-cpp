52c41e63f357fc052ecadea29dafc6c361049464
mourdani <meziane.ourdani@protonmail.com>
2022-11-08 02:26:10 +0100

    add ex01

--- day02/ex01/inc/Fixed.hpp
+++ day02/ex01/inc/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mourdani <mourdani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 23:19:52 by mourdani          #+#    #+#             */
-/*   Updated: 2022/11/07 23:22:56 by mourdani         ###   ########.fr       */
+/*   Updated: 2022/11/08 02:21:39 by mourdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 
 class Fixed {
     private:
@@ -28,11 +29,17 @@ class Fixed {
 
     public:
         Fixed();
+        Fixed(const int integer);
+        Fixed(const float floatn);
         Fixed(const Fixed &n);
         Fixed &operator=(const Fixed &n);
         ~Fixed();
 
+	int toInt( void ) const;
+	float toFloat( void ) const;
         int getRawBits( void ) const;
         void setRawBits( int const number );
 
 };
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
--- day02/ex01/src/Fixed.cpp
+++ day02/ex01/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: mourdani <mourdani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 23:19:48 by mourdani          #+#    #+#             */
-/*   Updated: 2022/11/07 23:19:50 by mourdani         ###   ########.fr       */
+/*   Updated: 2022/11/08 02:24:02 by mourdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,58 @@
 
 // Default constructor 
 Fixed::Fixed(): _fixed_number(0){
-    std::cout << "Default constructor called\n";
+	std::cout << "Default constructor called\n";
 };
 
 // Copy constructor 
 Fixed::Fixed(const Fixed &n){
-    std::cout << "Copy constructor called\n";
-    *this = n;
+	std::cout << "Copy constructor called\n";
+	*this = n;
 }
+
+// Int constructor 
+Fixed::Fixed(const int integer): _fixed_number(integer << this->_point) {
+	std::cout << "Int constructor called\n";
+};
+
+// Float constructor 
+Fixed::Fixed(const float fnumber): _fixed_number(roundf(fnumber * (1 << this->_point))) {
+	std::cout << "Float constructor called\n";
+};
 
 // Destructor
 Fixed::~Fixed(){
-    std::cout << "Deconstructor called\n";
+	std::cout << "Deconstructor called\n";
 };
 
 // Operator overload
 Fixed &Fixed::operator=(const Fixed &n){
-    std::cout << "Assignation operator called\n";
-    setRawBits(n.getRawBits());
-    return *this;
+	std::cout << "Assignation operator called\n";
+	setRawBits(n.getRawBits());
+	return *this;
+};
+
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed) {
+	out << fixed.toFloat();
+	return out;
 }
 
 // Returns the raw value of the fixed-point value
 int Fixed::getRawBits( void ) const{
-    std::cout << "getRawBits member function called\n";
-    return this->_fixed_number;
+	return this->_fixed_number;
 
-}
+};
 
 // Sets the raw value of the fixed-point number
 void Fixed::setRawBits( int const number ){
-    this->_fixed_number = number;
-}
+	this->_fixed_number = number;
+};
+
+int Fixed::toInt( void ) const {
+	return (this->_fixed_number >> this->_point);
+};
+
+float Fixed::toFloat( void ) const {
+	return ((float)this->_fixed_number / (1 << this->_point));
+};
