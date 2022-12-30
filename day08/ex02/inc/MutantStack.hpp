63d0b9c24ad206408e15a00f9b6fc7bba5ef4528
Ourdani Meziane <57743239+mourdani@users.noreply.github.com>
2022-12-30 14:36:38 +0000

    add ex02

--- day08/ex02/inc/MutantStack.hpp
+++ day08/ex02/inc/MutantStack.hpp
@@ -16,11 +16,13 @@ public:
 
 	MutantStack() : std::stack<T>() {};
 	MutantStack(MutantStack &copy) : std::stack<T>() {*this = copy;};
-	virtual ~MutantStack() { };
+	virtual ~MutantStack() {};
 
 	MutantStack	&operator=(MutantStack &other) {std::stack<T>::operator=(other); return (*this);};
 
-	iterator	end() {return(this->c.end());};
+	iterator	end() {
+		return(this->c.end());
+		};
 	iterator	begin() {return(this->c.begin());};
 };
 
--- day08/ex02/src/main.cpp
+++ day08/ex02/src/main.cpp
@@ -2,25 +2,91 @@
 
 int main()
 {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
+   	{
+		MutantStack<int> mstack;
 
-    while (it != ite)
-    {
-        std::cout << *it << std::endl;
-        ++it;
-    }
-    std::stack<int> s(mstack);
+		mstack.push(1);
+		mstack.push(2);
+
+		std::cout << "top: " << mstack.top() << std::endl;
+
+		mstack.pop();
+		std::cout << "top: " << mstack.top() << std::endl;
+
+		mstack.push(3);
+		mstack.push(4);
+		mstack.push(5);
+		mstack.push(6);
+
+		std::cout << "top: " << mstack.top() << std::endl;
+		std::cout << "size: " << mstack.size() << std::endl;
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		while(it != ite)
+		{
+			std::cout << BLUE << *it << RESET << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+
+		std::cout << "copy size: " << s.size() << std::endl;
+		std::cout << "original size: "<< mstack.size() << std::endl;
+
+		MutantStack<int>::reverse_iterator it_reverse = mstack.rbegin();
+		MutantStack<int>::reverse_iterator ite_reverse = mstack.rend();
+
+		while(it_reverse != ite_reverse)
+		{
+			std::cout << MAGENTA << *it_reverse << RESET << std::endl;
+			++it_reverse;
+		}
+	}
+	std::cout << "--------------" << std::endl;
+	{
+		MutantStack<int, std::list<int> > mstack;
+
+		mstack.push(1);
+		mstack.push(2);
+
+		std::cout << "top: " << mstack.top() << std::endl;
+
+		mstack.pop();
+		std::cout << "top: " << mstack.top() << std::endl;
+
+		mstack.push(3);
+		mstack.push(4);
+		mstack.push(5);
+		mstack.push(6);
+
+		std::cout << "top: " << mstack.top() << std::endl;
+		std::cout << "size: " << mstack.size() << std::endl;
+
+		MutantStack<int, std::list<int> >::iterator it = mstack.begin();
+		MutantStack<int, std::list<int> >::iterator ite = mstack.end();
+
+		++it;
+		--it;
+
+		while(it != ite)
+		{
+			std::cout << BLUE << *it << RESET << std::endl;
+			++it;
+		}
+		std::stack<int, std::list<int> > s(mstack);
+
+		std::cout << "copy size: " << s.size() << std::endl;
+		std::cout << "original size: "<< mstack.size() << std::endl;
+
+		MutantStack<int, std::list<int> >::reverse_iterator it_reverse = mstack.rbegin();
+		MutantStack<int, std::list<int> >::reverse_iterator ite_reverse = mstack.rend();
+
+		while(it_reverse != ite_reverse)
+		{
+			std::cout << MAGENTA << *it_reverse << RESET << std::endl;
+			++it_reverse;
+		}
+	}
     return 0;
 }
