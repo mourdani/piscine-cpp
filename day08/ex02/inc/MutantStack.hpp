cc94fb608473ddf6ae7dc71669d45d674171d617
mourdani <mourdani@student.42.fr>
2022-12-30 13:45:36 +0100

    add ex02

---
+++ day08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,27 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <deque>
+#include <algorithm>
+#include <stack>
+#include <iterator>
+#include <ostream>
+#include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+
+public:
+	typedef typename std::deque<T>::iterator iterator;
+
+	MutantStack() : std::stack<T>() {};
+	MutantStack(MutantStack &copy) : std::stack<T>() {*this = copy;};
+	virtual ~MutantStack() { };
+
+	MutantStack	&operator=(MutantStack &other) {std::stack<T>::operator=(other); return (*this);};
+
+	iterator	end() {return(this->c.end());};
+	iterator	begin() {return(this->c.begin());};
+};
+
+#endif
